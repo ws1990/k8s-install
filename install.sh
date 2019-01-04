bcbfe5a3e9efdf2ccc20d3df64aff20964c76afc
ws1990 <work_song@qq.com>
2019-01-04 14:10:34 +0800

    Update install.sh
    添加hostname的设置

--- install.sh
+++ install.sh
@@ -5,20 +5,34 @@ source ./kubernetes.conf
 # ip数组
 master_ip_arr=(${master_ip//,/ })
 node_ip_arr=(${node_ip//,/ })
-all_ip_arr=(${master_ip_arr[@]} ${node_ip_arr[@]} )
+all_ip_arr=(${master_ip_arr[@]} ${node_ip_arr[@]})
+hostname_arr=(${hostname//,/ })
 # 主节点IP
 first_master_ip=${master_ip_arr[0]}
 install_path=`pwd`
 
 
-# 1. 分发安装脚本到其它服务器
+# 1. 设置hostname
+for((i=0;i<${#all_ip_arr[@]};i++))
+do
+  echo "${all_ip_arr[$i]} ${hostname_arr[$i]}" >> /etc/hosts
+done
+hostnamectl set-hostname ${hostname_arr[0]}
+for((i=1;i<${#all_ip_arr[@]};i++))
+do
+  ssh root@${all_ip_arr[$i]} "hostnamectl set-hostname ${hostname_arr[$i]}"
+  scp /etc/hosts root@${all_ip_arr[$i]}:/etc/
+done
+
+
+# 2. 分发安装脚本到其它服务器
 for((i=1;i<${#all_ip_arr[@]};i++))
 do
   scp -r $install_path root@${all_ip_arr[$i]}:$install_path
 done
 
 
-# 2. 所有节点安装docker
+# 3. 所有节点安装docker
 # 主master直接执行
 ./docker_install.sh "${all_ip_arr[*]}" "1"
 # 其余节点远程执行
@@ -28,7 +42,7 @@ do
 done
 
 
-# 3. 所有master节点安装etcd
+# 4. 所有master节点安装etcd
 # 主master直接执行
 ./etcd_install.sh "${master_ip_arr[*]}" "${master_ip_arr[0]}" "1" "1"
 # 其余节点远程执行
@@ -39,7 +53,7 @@ do
 done
 
 
-# 4. master节点安装keepalived
+# 5. master节点安装keepalived
 uuid=`cat /proc/sys/kernel/random/uuid`
 # 主master直接执行
 ./keepalived_install.sh "${master_ip_arr[*]}" "${master_ip_arr[0]}" "1" "$load_balancer_dns" "$uuid"
@@ -50,7 +64,7 @@ do
 done
 
 
-# 5. master节点初始化kubelet
+# 6. master节点初始化kubelet
 # 主master直接执行
 ./master_install.sh "${master_ip_arr[*]}" "${node_ip_arr[*]}" "${master_ip_arr[0]}" "1" "$load_balancer_dns" "$load_balancer_port"
 # 其余节点远程执行
@@ -61,14 +75,14 @@ do
 done
 
 
-# 6. node节点加入集群
+# 7. node节点加入集群
 for((i=0;i<${#node_ip_arr[@]};i++))
 do
   ssh root@${node_ip_arr[$i]} "cd $install_path; ./node_install.sh; chmod +x join.sh; ./join.sh"
 done
 
 
-# 7. 最后验收，哇哈哈哈
+# 8. 最后验收，哇哈哈哈
 echo "检测是否安装成功："
 source ~/.bash_profile
 kubectl get pod --all-namespaces -o wide
